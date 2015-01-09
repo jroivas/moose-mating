125843752b8679f4b453d4f26efd8d9c16cea40c
Jouni Roivas <jroivas@iki.fi>
2015-01-09 21:22:19 +0200

    Doctest

--- moose.py
+++ moose.py
@@ -2,6 +2,7 @@ import random
 
 class Moose(object):
     def __init__(self, dna=''):
+        # XXX These can be removed...
         self.viewarea = 100
         self.movingchange = 2
         self.stillchange = 2
@@ -15,7 +16,9 @@ class Moose(object):
         self.searchingduration = 500
         self.shape = 0
         self.color = 0
+        # /XXX
 
+        self.alive = True
         self.age = 0
         self.dna = dna
 
@@ -26,15 +29,113 @@ class Moose(object):
         self.parents = []
 
     def tick(self):
+        """
+        >>> a = Moose()
+        >>> a.age
+        0
+        >>> a.age
+        0
+        >>> a.tick()
+        >>> a.age
+        1
+        >>> a.tick()
+        >>> a.age
+        2
+        """
         self.age += 1
 
     def die(self):
+        """
+        >>> a = Moose()
+        >>> a.alive
+        True
+        >>> a.die()
+        >>> a.alive
+        False
+        """
         self.alive = False
 
     def parseInt(self, val, base=2):
+        """
+        >>> a = Moose()
+        >>> a.parseInt('0')
+        0
+        >>> a.parseInt('1')
+        1
+        >>> a.parseInt('01')
+        1
+        >>> a.parseInt('10')
+        2
+        >>> a.parseInt('11')
+        3
+        >>> a.parseInt('011')
+        3
+        >>> a.parseInt('100')
+        4
+        >>> a.parseInt('101')
+        5
+        >>> a.parseInt('101', 10)
+        101
+        >>> a.parseInt('af', 16)
+        175
+        >>> a.parseInt('af', 10) # doctest: +ELLIPSIS
+        Traceback (most recent call last):
+        ...
+        ValueError: invalid literal for int() with base 10: 'af'
+        >>> a.parseInt('3', 2) # doctest: +ELLIPSIS
+        Traceback (most recent call last):
+        ...
+        ValueError: invalid literal for int() with base 2: '3'
+        """
         return int(val, base)
 
     def importdna(self, dna):
+        """
+        Example DNA:
+        00|11|000|111|000|1111|000|111|0000|111|000|111|0000|
+        sh co viw mov stl dirc men ens enmv mis mas jpf sedu
+
+        Shape: 1 of 4 shapes
+        Color: 1 of 4 color variations
+        Viewarea: 70 + DNA Value (0-7) * 15
+        Movingchange: 1 + DNA Value (0-7)
+        Stillchange: 1 + DNA Value (0-7)
+        Directionchange: DNA Value (0-15)
+        Maxenergy: 4 + DNA Value (0-7)
+        Energystill: 0.001 + DNA Value (0-7) / 2000
+        Minspeed: 0.5 + DNA Value (0-7) / 2
+        Maxspeed: Minspeed + DNA Value (0-7)/1.5
+        Energymoving: Energystill * (1 + maxspeed/10) * (1 + DNA Value (0-15)/15)
+        Jumpforce: 7 + DNA Value (0-7)
+        Searchduration: 300 + DNA Value (0-15)*100
+
+        >>> a = Moose()
+        >>> a.importdna('0' * 40)
+        >>> a.dna
+        '0000000000000000000000000000000000000000'
+        >>> a.viewarea
+        70
+        >>> a.movingchange
+        1
+        >>> a.minspeed
+        0.5
+        >>> a.maxspeed
+        0.5
+        >>> a.importdna('1' * 40)
+        >>> a.dna
+        '1111111111111111111111111111111111111111'
+        >>> a.viewarea
+        175
+        >>> a.movingchange
+        8
+        >>> a.minspeed
+        3.5
+        >>> a.maxspeed # doctest: +ELLIPSIS
+        8.1...
+        >>> a.importdna('0011000111000111100011100001110001110000')
+        >>> a.dna
+        '0011000111000111100011100001110001110000'
+        """
         if len(dna) < 40:
             return
 
@@ -55,9 +156,34 @@ class Moose(object):
         self.searchingduration = 300 + self.parseInt(dna[36:40]) * 100
 
     def randomBool(self):
+        """
+        """
         return bool(random.getrandbits(1))
 
     def combine(self, another):
+        """
+        >>> a = Moose('0' * 40)
+        >>> b = Moose('1' * 40)
+        >>> bak1 = random.getrandbits
+        >>> bak2 = random.randint
+        >>> random.getrandbits = lambda x: 0
+        >>> random.randint = lambda x, y: (y - x) / 2
+        >>> c = a.combine(b)
+        >>> c.dna != b.dna
+        True
+        >>> c.dna != a.dna
+        True
+        >>> random.getrandbits = lambda x: 1
+        >>> d = a.combine(b)
+        >>> c.dna != d.dna
+        True
+        >>> c.dna.count('0') > 1
+        True
+        >>> c.dna.count('1') > 1
+        True
+        >>> random.getrandbits = bak1
+        >>> random.randint = bak2
+        """
         point = random.randint(0, 39)
 
         if self.randomBool():
@@ -74,6 +200,19 @@ class Moose(object):
         return child
 
     def mutate(self, rawdna):
+        """
+        >>> a = Moose('0' * 40)
+        >>> rnd = random.randint
+        >>> random.randint = lambda x, y: 1
+        >>> dna = a.mutate(a.dna)
+        >>> '1' in dna
+        True
+        >>> random.randint = lambda x, y: 0
+        >>> dna = a.mutate(a.dna)
+        >>> '1' in dna
+        False
+        >>> random.randint = rnd
+        """
         dna = ''
         for c in rawdna:
             if random.randint(1, 100) == 1:
@@ -87,9 +226,23 @@ class Moose(object):
         return dna
 
     def randomizeName(self):
+        """
+        >>> Moose().name != Moose().name
+        True
+        >>> Moose().name != Moose().name
+        True
+        """
         self.name = 'Moose-%s' % (random.randint(1, 99999))
 
     def randomize(self):
+        """
+        >>> a = Moose()
+        >>> b = Moose()
+        >>> a.randomize()
+        >>> b.randomize()
+        >>> a.dna != b.dna
+        True
+        """
         self.importdna(''.join(['%s' % random.getrandbits(1) for x in xrange(40)]))
 
     def __str__(self):
@@ -99,8 +252,49 @@ class Moose(object):
         return '%s %s' % (self.name, self.dna)
 
     def info(self):
+        """
+        >>> a = Moose('0' * 40)
+        >>> b = Moose('1' * 40)
+        >>> print a.info() # doctest: +ELLIPSIS
+        Name            : Moose-...
+        Age             : 0
+        Alive           : True
+        Shape           : 00
+        Color           : 00
+        View area       : 70
+        Moving change   : 1
+        Still change    : 1
+        Direction change: 0
+        Max energy      : 4
+        Jump force      : 7
+        Energy still    : 0.001
+        Energy moving   : 0.00105
+        Max speed       : 0.5
+        Min speed       : 0.5
+        Search duration : 300
+        DNA             : 0000000000000000000000000000000000000000
+        >>> print b.info() # doctest: +ELLIPSIS
+        Name            : Moose-...
+        Age             : 0
+        Alive           : True
+        Shape           : 11
+        Color           : 11
+        View area       : 175
+        Moving change   : 8
+        Still change    : 8
+        Direction change: 15
+        Max energy      : 11
+        Jump force      : 14
+        Energy still    : 0.001
+        Energy moving   : 0.0036...
+        Max speed       : 8.16...
+        Min speed       : 3.5
+        Search duration : 1800
+        DNA             : 1111111111111111111111111111111111111111
+        """
         return """Name            : %s
 Age             : %s
+Alive           : %s
 Shape           : %s
 Color           : %s
 View area       : %s
@@ -114,10 +308,10 @@ Energy moving   : %s
 Max speed       : %s
 Min speed       : %s
 Search duration : %s
-DNA             : %s
-        """ % (
+DNA             : %s""" % (
             self.name,
             self.age,
+            self.alive,
             self.shape,
             self.color,
             self.viewarea,
