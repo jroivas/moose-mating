83821ba013ef625aac36d4ff90c7bf6d6b411b1d
Jouni Roivas <jroivas@iki.fi>
2015-01-10 11:52:37 +0200

    Argparse, able to change any parameter

--- orgy.py
+++ orgy.py
@@ -1,15 +1,16 @@
 #!/usr/bin/env python
 
+import argparse
 import moose
 import random
 import sys
 
 
-def gen(m1, m2, maxchild=3):
+def gen(m1, m2, maxchild=3, algorithm=''):
     child = []
     print ('Mating: %s and %s' % (m1.name, m2.name))
     for x in range(random.randint(1, maxchild + 1)):
-        child.append(m1.combine(m2))
+        child.append(m1.combine(m2, algorithm))
 
     return child
 
@@ -23,16 +24,16 @@ def maters(mooses):
         if a != b:
             return (a, b)
 
-def mate_two(mooses):
+def mate_two(mooses, maxchild=3, algorithm=''):
     res = maters(mooses)
     if res is None:
         return []
-    return gen(res[0], res[1])
+    return gen(res[0], res[1], maxchild=maxchild, algorithm=algorithm)
 
-def orgy(mooses, pairs):
+def orgy(mooses, pairs, maxchild=3, algorithm=''):
     res = []
     for x in range(int(pairs)):
-        res += mate_two(mooses)
+        res += mate_two(mooses, maxchild=maxchild, algorithm=algorithm)
     return mooses + res
 
 def tick(mooses):
@@ -40,28 +41,39 @@ def tick(mooses):
         m.tick()
 
 if __name__ == '__main__':
-    if len(sys.argv) >= 3:
-        zero = moose.Moose(sys.argv[1])
-        one  = moose.Moose(sys.argv[2])
-    else:
-        zero = moose.Moose('0' * 40)
-        one  = moose.Moose('1' * 40)
+    parser = argparse.ArgumentParser(description='Moose orgy')
+    parser.add_argument('mooses', nargs='*', help='Original mooses')
+    parser.add_argument('-r', '--rate', default=100, type=int, help='Mutation rate')
+    parser.add_argument('-g', '--generations', default=10, type=int, help='Number of iterations (min 1)')
+    parser.add_argument('-c', '--max-child', default=3, type=int, help='Maximum number of children')
+    parser.add_argument('-p', '--pairs', default=1.5, type=float, help='Orgy pair divide factor, can\'t be 0')
+    parser.add_argument('-m', '--mutation', default='default', help='Mutation algorithm')
 
-    # Parent mooses
-    mooses = [zero, one]
-    tick(mooses)
+    res = parser.parse_args()
+    args = vars(res)
 
-    # First generation of moose
-    mooses += gen(zero, one)
+    if args['pairs'] == 0 or args['generations'] < 1:
+        parser.print_help()
+        sys.exit(1)
+
+    mooses = []
+    for dna in args['mooses']:
+        mooses.append(moose.Moose(dna, args['rate']))
+
+    if len(mooses) < 2:
+        mooses.append(moose.Moose('0' * 40, args['rate']))
+    if len(mooses) < 2:
+        mooses.append(moose.Moose('1' * 40, args['rate']))
+
     tick(mooses)
 
     cnt = 0
-    maxcnt = 10
+    maxcnt = args['generations']
     while cnt < maxcnt:
-        mooses = orgy(mooses, max(1, cnt/1.5))
+        mooses = orgy(mooses, max(1, cnt / args['pairs']), maxchild=args['max_child'], algorithm=args['mutation'])
         print ('gen %s: %s' % (cnt + 1, mooses))
         tick(mooses)
         cnt += 1
 
     for moo in mooses:
-        print moo, moo.parents
+        print '%s parents: %s' % (moo, moo.parents)
